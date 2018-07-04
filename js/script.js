a95126ba1113e7564b04e183800dd6889bc2a916
Aleksey <Mr.fistery@mail.ru>
2018-07-05 01:26:30 +0600

    last

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 var link = document.querySelector(".map-button-info");
 var modalmap = document.querySelector(".modal-page-map");
-var close = document.querySelector(".modal-close");
-var login = modalmap.querySelector(".name-form");
-var email = modalmap.querySelector(".one-form");
-var textarea = modalmap.querySelector(".modal-textarea");
-var form = modalmap.querySelector("form");
+if(modalmap) {
+  var login = modalmap.querySelector(".name-form");
+  var email = modalmap.querySelector(".one-form");
+  var textarea = modalmap.querySelector(".modal-textarea");
+  var form = modalmap.querySelector("form");
+  var close = document.querySelector(".modal-close");
+  var login = modalmap.querySelector(".name-form");
+  var email = modalmap.querySelector(".one-form");
+  var textarea = modalmap.querySelector(".modal-textarea");
+  var form = modalmap.querySelector("form");
+}
 var buyup = document.querySelectorAll(".green-back-button");
 var buypage = document.querySelector(".buy-modal-page");
 var buyClose = document.querySelector(".link-modal-exit");
 var priceClose = document.querySelector(".modal-price-link");
-var sliderLinkOne = document.querySelector(".slide-button-one");
-var sliderOne = document.querySelector(".one-slide");
-var sliderLinkTwo = document.querySelector(".slide-button-two");
-var sliderTwo = document.querySelector(".two-slide");
-var sladerRight = document.querySelector(".main-slader-left-link");
-var sliderLeft = document.querySelector(".main-slider-right-link");
+var mainSlider = document.querySelector(".main-slider");
+if(mainSlider) {
+  var sliderLinkOne = document.querySelector(".slide-button-one");
+  var sliderOne = document.querySelector(".one-slide");
+  var sliderLinkTwo = document.querySelector(".slide-button-two");
+  var sliderTwo = document.querySelector(".two-slide");
+  var sliderRight = document.querySelector(".main-slader-left-link");
+  var sliderLeft = document.querySelector(".main-slider-right-link");
+}
+
+var PopupMap = document.querySelector(".popup-map");
+if(PopupMap) {
+var PopupMapLink = document.querySelector(".map-button");
+var PopupExit = PopupMap.querySelector(".link-modal-exit-popup");
+}
 
 for (let i = 0; i < buyup.length; i++) {
   buyup[i].addEventListener("click", function (evt) {
@@ -33,7 +48,7 @@ priceClose.addEventListener("click", function (evt){
 buypage.classList.remove("modal-up");
 })
 
-sladerRight.addEventListener("click", function (evt){
+sliderRight.addEventListener("click", function (evt){
   evt.preventDefault();
   sliderOne.classList.toggle("slide-display");
   sliderTwo.classList.toggle("slide-display");
@@ -75,17 +90,17 @@ form.addEventListener("submit", function () {
   }
 });
 
-buyup.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  buypage.classList.add("modal-up");
-});
-
-buyClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  buypage.classList.remove("modal-up");
-});
-
 priceClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   buypage.classList.remove("modal-up");
 });
+
+PopupMapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  PopupMap.classList.add("modal-up");
+})
+
+PopupExit.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  PopupMap.classList.remove("modal-up");
+})
